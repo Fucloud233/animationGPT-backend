9afae76eee4763f138f6bb7370f7aa233676dd4a
Fucloud-arch <270928474@qq.com>
2024-03-06 13:12:34 +0800

    feat: basic generate api

--- server/main.py
+++ server/main.py
@@ -1,10 +1,14 @@
 from enum import Enum
-from flask import Flask, request
+from flask import Flask, request, send_from_directory, send_file
+from pathlib import Path
 
 from utils.translate import translate
 from utils.hash import hash_string
+from store import Store
+from bot import T2MBot
 
 app = Flask("animationGPT")
+bot = T2MBot()
 
 class LangKind(Enum):
     EN = 'en'
@@ -12,35 +16,35 @@ class LangKind(Enum):
 
 @app.route("/generate", methods=['POST'])
 def generate():
-    # 1. 输入 Prompt，选择生成语言
+    # 1. 输入 Prompt，并选择输入的语言
     try:
         prompt = request.json['prompt']
         lang = LangKind(request.json['language'])
 
-        print(prompt, lang)
+        # print(prompt, lang)
     except KeyError:
         return "key parameter not found"
     except ValueError:
         return "the kind of language not supported"
 
-    # 3. 后端接收 Prompt，如果是中文，调用 API 翻译成英文
+    # 3. 如果输入语言不为英文，则需要调用API翻译
     if lang != LangKind.EN:
         (flag, prompt) = translate(prompt)
         if not flag:
             return "translate error"
         
 
-    # 4. 通过 Hash 算法生成一个 id，将 id 和 prompt 保存到 Redis 中
-    hash_code = hash_string(prompt)
-
-    return hash_code
+    # 4. 通过hash算法将Prompt转换称为16进制，并存储起来
+    id = hash_string(prompt)
     
+    if not Store.check_exist(id):
+        Store.set(id, prompt)
+        bot.generate_motion(prompt, id)
 
-    # > Hash 算法和输入的语言有关，因此需要考虑语言分类
-
-    # redis 注意保持会话 id 的问题
-
-    return prompt
+    # 5. 最后返回视频
+    path = Path.joinpath(Path("cache"), id, "video.mp4")
+    
+    return send_file(path)
 
 
 @app.route("/download", methods=['GET'])
