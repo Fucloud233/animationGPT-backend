980868f262c980d87f13a1de5d4572c638da6bdc
Fucloud-arch <270928474@qq.com>
2024-03-07 11:34:11 +0800

    fix: replace matrix_multiply to matmul

--- server/utils/npy2bvh/visualization/Animation.py
+++ server/utils/npy2bvh/visualization/Animation.py
@@ -1,7 +1,7 @@
 import operator
 
 import numpy as np
-import numpy.core.umath_tests as ut
+# import numpy.core.umath_tests as ut
 
 from visualization.Quaternions import Quaternions
 
@@ -223,7 +223,8 @@ def transforms_multiply(t0s, t1s):
         together
     """
 
-    return ut.matrix_multiply(t0s, t1s)
+    # return ut.matrix_multiply(t0s, t1s)
+    return np.matmul(t0s, t1s)
 
 
 def transforms_inv(ts):
